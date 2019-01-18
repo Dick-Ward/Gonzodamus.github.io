bd788e5644a5c6b4f723783bb44f01a35ceb7391
gonzodamus <github email address>
2019-01-18 18:59:36 -0500

    commit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const countdown = document.getElementById("countdown")
 const targetDate = new Date('Jan 18, 2020')
-console.log(targetDate)
+const birthday = false
 
 
 
@@ -16,17 +16,31 @@ let hours = minutes * 60;
 let days = hours * 24;
 let years = days * 365
 displayDays = Math.round(time / years)
-countdown.innerHTML = displayDays
 days_left = (Math.round((targetDate.getTime() - date.getTime())/days))
 minutes_left = (Math.round((targetDate.getTime() - date.getTime())/minutes))
 seconds_left = (Math.round((targetDate.getTime() - date.getTime())/time))
-
 console.log(days_left)
-const clockCount = () =>{
-    setInterval( () =>{
-        time--
-        countdown.innerHTML = displayDays
-    }, 1000)
+
+
+
+const checkBirthday = () =>{
+    if (date.getMonth() === 0 && date.getDate() === 18){
+        return "YES!  Happy birthday Joey!"
+    } else {
+        return `Nope! But only ${days_left} to go!`
+    }
 }
-clockCount()
+
+countdown.innerHTML = checkBirthday()
+
+
+
+// console.log(days_left)
+// const clockCount = () =>{
+//     setInterval( () =>{
+//         time--
+//         countdown.innerHTML = displayDays
+//     }, 1000)
+// }
+// clockCount()
 

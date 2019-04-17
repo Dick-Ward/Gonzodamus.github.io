d5c1be5fec1c155dd3ab56a37fa0d2909c3c456c
Gonzodamus <Dick@TheDickWard.com>
2019-04-17 15:35:45 -0400

    cleaned up excess variables

--- index.js
+++ index.js
@@ -1,31 +1,12 @@
+let nextBirthdayYear = 2020
 const countdown = document.getElementById("countdown")
-const targetDate = new Date('Jan 18, 2020')
-const birthday = false
-
-
-
-
-
+const targetDate = new Date(`Jan 18, ${nextBirthdayYear}`)
 let date = new Date()
-
-
-let time = date.getTime()
-let minutes = 1000 * 60;
-let hours = minutes * 60;
-let days = hours * 24;
-let years = days * 365
-displayDays = Math.round(time / years)
-days_left = (Math.round((targetDate.getTime() - date.getTime())/days))
-minutes_left = (Math.round((targetDate.getTime() - date.getTime())/minutes))
 seconds_left = (Math.round((targetDate.getTime() - date.getTime())/1000))
-console.log(seconds_left)
-
-
-
-
 
 const checkBirthday = () =>{
     if (date.getMonth() === 0 && date.getDate() === 18){
+        console.log(nextBirthdayYear)
         return "YES!  Happy birthday Joey!"
     } else {
         return `Nope! But only ${seconds_left} seconds to go until the next one!`
